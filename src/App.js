5f43ff8f4deb3c84dde43e3c9ca35f8b4f655bd2
ckschlosser <ckschlosser13@gmail.com>
2022-12-14 16:52:41 -0600

    added footer section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Sections from "./components/Sections";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Hero />
       <Sections />
+      <Footer />
     </div>
   );
 };
