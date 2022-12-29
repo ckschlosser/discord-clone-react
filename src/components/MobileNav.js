e44c172853c70a90cc41eafd8a5656f3404bd58e
ckschlosser <ckschlosser13@gmail.com>
2022-12-29 14:48:50 -0600

    added mobile nav function and hover functionality on various elements

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -6,7 +6,7 @@ import MobileNavBtn from "./icons/MobileNavBtn";
 
 function MobileNav() {
   return (
-    <div className="relative box-border w-[330px] overflow-hidden z-50 bg-white h-screen rounded-tl-lg rounded-bl-lg pl-6 pt-6 pr-12 pb-[120px] overflow-y-scroll -left-2.5">
+    <div className="fixed box-border w-[330px] overflow-hidden z-50 bg-white h-screen rounded-tl-lg rounded-bl-lg pl-6 pt-6 pr-12 pb-[120px] overflow-y-scroll top-0 right-0">
       <div className="w-full">
         <nav className="flex justify-between w-100%">
           <DiscordImg fill="#23272a" />
@@ -15,35 +15,38 @@ function MobileNav() {
         <nav className="text-darkGrey font-sans font-light">
           <a
             href="#"
-            className="flex py-2 px-4 text-lightestBlue bg-lightGrey rounded-md"
+            className="flex py-2 px-4 text-lightestBlue bg-lightGrey rounded-md hover:underline"
           >
             Home
           </a>
-          <a href="#" className="flex py-2 px-4 text-black">
+          <a href="#" className="flex py-2 px-4 text-black hover:underline">
             Download
           </a>
-          <a href="#" className="flex py-2 px-4 text-black">
+          <a href="#" className="flex py-2 px-4 text-black hover:underline">
             Nitro
           </a>
-          <a href="#" className="flex py-2 px-4 text-black">
+          <a href="#" className="flex py-2 px-4 text-black hover:underline">
             Discover
           </a>
-          <a href="#" className="flex items-center py-2 px-4 text-black">
+          <a
+            href="#"
+            className="flex items-center py-2 px-4 text-black hover:underline"
+          >
             <div className="flex">Safety</div>
 
             <CarrotRight className="-rotate-90 ml-2" />
           </a>
-          <a href="#" className="flex py-2 px-4 text-black">
+          <a href="#" className="flex py-2 px-4 text-black hover:underline">
             <div className="flex">Mod Academy</div>
             <CarrotRight className="-rotate-90 ml-2" />
           </a>
-          <a href="#" className="flex py-2 px-4 text-black">
+          <a href="#" className="flex py-2 px-4 text-black hover:underline">
             Support
           </a>
           <a href="#" className="flex py-2 px-4 text-black">
             Blog
           </a>
-          <a href="#" className="flex py-2 px-4 text-black">
+          <a href="#" className="flex py-2 px-4 text-black hover:underline">
             Careers
           </a>
         </nav>
@@ -51,7 +54,9 @@ function MobileNav() {
         <button className="fixed top-0 right-0 m-6 z-[80]">
           <CloseX />
         </button>
-        <MobileNavBtn />
+        <div className="fixed right-0 bottom-0 p-6 h-[120px] w-[330px] flex justify-center flex-col z-[80] bg-white rounded-bl-lg">
+          <MobileNavBtn />
+        </div>
       </div>
     </div>
   );
--- src/components/icons/MobileNavBtn.js
+++ src/components/icons/MobileNavBtn.js
@@ -2,17 +2,15 @@ import React from "react";
 
 function MobileNavBtn() {
   return (
-    <div className="fixed left-0 right-0 bottom-0 p-6 h-[120px] flex justify-end flex-col z-[80] bg-white">
-      <button className="flex justify-center rounded-3xl py-[7px] px-4 text-sm leading-6 bg-mainBlue text-white">
-        <svg width="24" height="24" viewBox="0 0 24 24" className="mr-2">
-          <g fill="currentColor">
-            <path d="M17.707 10.708L16.293 9.29398L13 12.587V2.00098H11V12.587L7.70697 9.29398L6.29297 10.708L12 16.415L17.707 10.708Z"></path>
-            <path d="M18 18.001V20.001H6V18.001H4V20.001C4 21.103 4.897 22.001 6 22.001H18C19.104 22.001 20 21.103 20 20.001V18.001H18Z"></path>
-          </g>
-        </svg>
-        Download from Google Play
-      </button>
-    </div>
+    <button className="flex justify-center rounded-3xl py-[7px] px-4 text-sm leading-6 bg-mainBlue text-white hover:duration-500 hover:shadow-lg hover:bg-mainBlue/80">
+      <svg width="24" height="24" viewBox="0 0 24 24" className="mr-2">
+        <g fill="currentColor">
+          <path d="M17.707 10.708L16.293 9.29398L13 12.587V2.00098H11V12.587L7.70697 9.29398L6.29297 10.708L12 16.415L17.707 10.708Z"></path>
+          <path d="M18 18.001V20.001H6V18.001H4V20.001C4 21.103 4.897 22.001 6 22.001H18C19.104 22.001 20 21.103 20 20.001V18.001H18Z"></path>
+        </g>
+      </svg>
+      Download from Google Play
+    </button>
   );
 }
 
